a6e1779bd4c30e8fcbfbc456cf33977fd78a014d
Chara Oberste-Lehn <doberste@student.42vienna.com>
2025-10-21 14:15:29 +0200

    added comment about weird memmove behavior

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: doberste <doberste@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/01 13:04:01 by doberste          #+#    #+#             */
-/*   Updated: 2025/10/08 16:02:09 by doberste         ###   ########.fr       */
+/*   Updated: 2025/10/21 14:13:43 by doberste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,3 +38,10 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	}
 	return ((void *)dest);
 }
+// #include <string.h>
+// int main()
+// {
+//
+// null check behaves weirdly on different systems
+// 	printf("%p", memmove("", NULL, 3));
+// }
